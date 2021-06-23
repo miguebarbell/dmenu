e317188a3cb88b6b077871a35f3fa7bae2f08707
Migue <migue@tuta.io>
2021-06-23 15:28:15 -0400

    added just the  monitor availables as options
    Took 2 minutes

--- dmonitor/dmonitor.py
+++ dmonitor/dmonitor.py
@@ -1,19 +1,34 @@
-#!/usr/bin/env python
+# !/usr/bin/python
+
 import subprocess, re, os
 
-where, output, resolution, actual, what = 'right-of', 'HDMI-1', '1920x1080', 'eDP-1', 'same-as'
+where = 'right-of'
+output = 'HDMI1'
+resolution = '1920x1080'
+what = 'same-as'
+actual = "eDP1"
 
+# TODO: extraer las resoluciones del monitor que se seleccione
+# TODO: documentatar las funciones
 
-def getResolutions():
+# def getResolutions():
+#     monitorRaw = subprocess.check_output('xrandr', shell=True)
+#     print(monitorRaw.decode())
+#     monitorClear = re.findall(r"n(\w+(-\d{0,2}-\d{0,1}))", str(monitorRaw))
+#     return monitorClear
+
+def getDisconnectMonitor():
     monitorRaw = subprocess.check_output('xrandr', shell=True)
-    monitorClear = re.findall(r"n(\w+(-\d{0,2}-\d{0,1})", str(monitorRaw))
-    return monitorClear
-
+    monitorClear = re.findall(r"(\w+)\sconnected\s\d", monitorRaw.decode())
+    return [monitorClear[i] for i in range(0, len(monitorClear))]
 
 def getMonitor():
     monitorRaw = subprocess.check_output('xrandr', shell=True)
-    monitorClear = re.findall(r"n(\w+((-\d{0,2}){1}(-\d{0,1}){0,1}))", str(monitorRaw))
-    return [monitorClear[i][0] for i in range(0, len(monitorClear))]
+    # print(f"monitor raws: {monitorRaw}")
+    monitorClear = re.findall(r"(\w+)\sconnected\s\(", monitorRaw.decode())
+    # monitorClear = re.findall(r"n(\w+((-\d{0,2}){1}(-\d{0,1}){0,1}))", str(monitorRaw))
+    # print(f'monitors founded {monitorClear}')
+    return [monitorClear[i] for i in range(0, len(monitorClear))]
 
 
 def dmenuWhere():
@@ -24,6 +39,7 @@ def dmenuWhere():
         position = "echo -e 'left-of\nright-of\nabove\nbelow' | dmenu -p 'Where place the Duplicate Monitor?' | xargs -I % echo '%'"
         ansPosition = subprocess.check_output(position, shell=True)
         where = re.findall(r"b'(\w+-*\w+)\\n'", str(ansPosition))[0]
+        # where = ansPosition.decode()
         what = 'Extra'
     elif 'Disconnect' in str(ansArguments):
         what = 'Disconnect'
@@ -36,6 +52,7 @@ def dmenuResolution():
     resolutionArgs = "echo -e '2K\n4K\nFHD\nHD\nAnother resolution' | dmenu -p 'Select the output resolution' | xargs -I % echo '%'"
     ansResolutionArgs = subprocess.check_output(resolutionArgs, shell=True)
     ansResolutionArgsClean = re.findall(r"b'(\w+\s*\w*)\\n", str(ansResolutionArgs))
+    # ansResolutionArgsClean = re.findall(r"b'(\w+\s*\w*)\\n", str(ansResolutionArgs))
     if 'Another resolution' in ansResolutionArgsClean:
         anotherResolutionArg = "echo -e | dmenu -p 'Which resolution? ex: 680x550' | xargs -I % echo '%'"
         anotherResolution = subprocess.check_output(anotherResolutionArg, shell=True)
@@ -51,27 +68,42 @@ def dmenuResolution():
 
 
 
-def dmenuOutput():
+def dmenuOutput(outputs):
     global output
-    monitors = str('\n'.join(getMonitor()))
+    monitors = str('\n'.join(outputs))
     arguments = subprocess.check_output(f"echo -e '{monitors}' | dmenu -p 'Which output?' | xargs -I % echo '%'", shell=True)
-    output = str(re.findall(r"b'(\w+-\d+)\\n'", str(arguments))[0])
+    # print(f"selected {arguments.decode()} oooo")
+    output = str(re.findall(r"b'(\w+\d+)\\n'", str(arguments))[0])
+    # output = arguments.decode("UTF-8")
+
+
 
 
 def allTogether():
     dmenuWhere()
-    dmenuOutput()
     if what == 'Disconnect':
+
+        dmenuOutput(getDisconnectMonitor())
         os.system(f"xrandr --output {output} --off")
         os.system(f"echo -e | dmenu -p 'Shutting down {output}...'")
     else:
+
+        dmenuOutput(getMonitor())
         dmenuResolution()
         if what == 'Extra':
-            os.system(f'xrandr --output {output} --set audio force-dvi --mode {resolution} && xrandr --output {actual} --auto --output {output} --{where} {actual}')
+            # os.system(f'xrandr --output {output} --mode {resolution} --{where} {actual}')
+            final = f'xrandr --output {output} --mode {resolution} --{where} {actual}'
+            # print(final)
+            subprocess.call(final, shell=True)
+            # os.system(f'xrandr --output {output} --set audio force-dvi --mode {resolution} && xrandr --output {actual} --auto --output {output} --{where} {actual}')
             os.system(f"echo -e | dmenu -p 'Adding {output} at {resolution} {where} {actual}'")
         else:
-            os.system(f"xrandr --output {output} --set audio force-dvi --mode {resolution} && xrandr --output {actual} --auto --output {output} --same-as {actual}")
+            # os.system(f"xrandr --output {output} --set audio force-dvi --mode {resolution} && xrandr --output {actual} --auto --output {output} --same-as {actual}")
+            os.system(f"xrandr --output {output} --mode {resolution} --same-as {actual}")
             os.system(f"echo -e | dmenu -p 'Cloning {actual} to {output} at {resolution}'")
 
 
 allTogether()
+
+
+
