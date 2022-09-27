6cb905ccaa03271f2a5a9ae81090b48aa6441885
Miguel R <miguel@debloat.us>
2022-09-27 12:01:39 -0400

    this is twice solved

--- dtgx/tgx.bash
+++ dtgx/tgx.bash
@@ -23,11 +23,11 @@ function search_tgx() {
   elif [[ $2 == 'Training' ]]; then
     PAGE_RESULTS=$(curl -s "$URLTGX/torrents.php?c33=1&search=$QUERY&lang=0&nox=2&sort=seeders&order=desc")
   fi
-  RESULTS=$(echo $PAGE_RESULTS | pup 'div#click.tgxtablecell div a' json{} | jq 'map(select(.class == "txlight"))')
-  TITLE_SELECTED=$(echo $RESULTS | jq '.[].title' | dmenu -p 'Results:' -l 10)
+  RESULTS=$(echo "$PAGE_RESULTS" | pup 'div#click.tgxtablecell div a' json{} | jq 'map(select(.class == "txlight"))')
+  TITLE_SELECTED=$(echo "$RESULTS" | jq '.[].title' | dmenu -p 'Results:' -l 10)
   if [[ $"$TITLE_SELECTED" == "" ]]; then exit 0; fi
-  LINK_SELECTED=$(echo $RESULTS | jq "map(select(.title == $TITLE_SELECTED)) | .[].href" | sed s'/"//g')
-  MAGNET_LINK=$(curl -s $URLTGX$LINK_SELECTED | pup 'center a.txlight.lift.btn.btn-danger' json{} | jq '.[2].href' | sed s'/"//g')
+  LINK_SELECTED=$(echo "$RESULTS" | jq "map(select(.title == $TITLE_SELECTED)) | .[].href" | sed s'/"//g')
+  MAGNET_LINK=$(curl -s "$URLTGX""$LINK_SELECTED" | pup 'center a.txlight.lift.btn.btn-danger' json{} | jq '.[1].href' | sed s'/"//g')
   if [[ 'Documentaries' == "$2"  || 'Movies' == "$2" || 'TV' == "$2" ]]; then
     WHAT=$(echo -e "Download\nStream\nCancel" | dmenu -p "What?")
     if [[ $WHAT == "Stream" ]]; then
@@ -39,6 +39,7 @@ function search_tgx() {
   fi
   echo -e | dmenu -p "Downloading $TITLE_SELECTED"
 
+	echo $MAGNET_LINK
   addtorrent "$MAGNET_LINK"
   #transmission-remote -a $MAGNET_LINK
   exit 0
